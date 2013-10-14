9e7a29d2ec0e553b0071cb695336db9ccc0e82ad
tonygurnick <tony.gurnick@gmail.com>
2013-10-15 00:24:13 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 selenium-standalone-wrapper
 ===========================
+
+Just a wrapper so the jar can be included as a dependency
---
+++ index.js
@@ -0,0 +1,8 @@
+var path=require("path");
+
+
+	module.exports=function(){
+		return path.resolve("./selenium-server-standalone-2.35.0.jar")
+	};
+
+
